94b7a0183bd2c9cd73ce9625b4dfe449ec1f3740
mai1209 <mairacoria1209@gmail.com>
2024-01-17 17:15:04 -0300

    super change

--- js.js
+++ js.js
@@ -1,7 +1,13 @@
 const openBtn = document.getElementById('btn1')
 const openBtn2 = document.getElementById('btn2')
+const openBtn3 = document.getElementById('btn3')
+const openBtn4 = document.getElementById('btn4')
+
 const info1 = document.getElementById('info1')
 const info2 = document.getElementById('info2')
+const info3 = document.getElementById('info3')
+const info4 = document.getElementById('info4')
+
 const btnMenu = document.getElementById('btn-menu')
 const activo = document.getElementById('active')
 
@@ -14,6 +20,16 @@ openBtn.addEventListener('click',() =>{
 openBtn2.addEventListener('click',() =>{
     info2.classList.toggle('open')
 } )
+
+openBtn3.addEventListener('click',() =>{
+    info3.classList.toggle('open')
+} )
+
+openBtn4.addEventListener('click',() =>{
+    info4.classList.toggle('open')
+} )
+
+
 
 //funcion para abrir el menu con btn hamburguesa
 
