4f427c3e2dc3b2c679d91db9ba91d64340416ed3
mai1209 <mairacoria1209@gmail.com>
2024-02-07 12:55:17 -0300

    cambias tratamientos

--- js.js
+++ js.js
@@ -2,17 +2,27 @@ const openBtn = document.getElementById('btn1')
 const openBtn2 = document.getElementById('btn2')
 const openBtn3 = document.getElementById('btn3')
 const openBtn4 = document.getElementById('btn4')
+const openBtn5 = document.getElementById('btn5')
+const openBtn6 = document.getElementById('btn6')
+const openBtn7 = document.getElementById('btn7')
+const openBtn8 = document.getElementById('btn8')
+const openBtn9 = document.getElementById('btn9')
 
 const info1 = document.getElementById('info1')
 const info2 = document.getElementById('info2')
 const info3 = document.getElementById('info3')
 const info4 = document.getElementById('info4')
+const info5 = document.getElementById('info5')
+const info6 = document.getElementById('info6')
+const info7 = document.getElementById('info7')
+const info8 = document.getElementById('info8')
+const info9 = document.getElementById('info9')
 
 const btnMenu = document.getElementById('btn-menu')
 const activo = document.getElementById('active')
 const producto= document.getElementById('producto')
 const submit= document.getElementById('submit')
-const formulario= document.getElementById('formulario')
+
 
 
 
@@ -33,6 +43,21 @@ openBtn3.addEventListener('click',() =>{
 
 openBtn4.addEventListener('click',() =>{
     info4.classList.toggle('open')
+} )
+openBtn5.addEventListener('click',() =>{
+    info5.classList.toggle('open')
+} )
+openBtn6.addEventListener('click',() =>{
+    info6.classList.toggle('open')
+} )
+openBtn7.addEventListener('click',() =>{
+    info7.classList.toggle('open')
+} )
+openBtn8.addEventListener('click',() =>{
+    info8.classList.toggle('open')
+} )
+openBtn9.addEventListener('click',() =>{
+    info9.classList.toggle('open')
 } )
 
 
